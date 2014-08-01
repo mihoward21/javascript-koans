4d221cd935be7257f79e7fe7e8c851a58cfe3866
Mike <mikeh09@gmail.com>
2014-08-01 18:14:05 -0400

    AboutFunctions passes all the tests

--- koans/AboutFunctions.js
+++ koans/AboutFunctions.js
@@ -103,7 +103,7 @@ describe("About Functions", function() {
       //An internal comment
       return a * b;
     };
-    expect(multiply.toString()).toBe("function (a, b) {\n//An internal comment\nreturn a * b;\n}");
+    expect(multiply.toString()).toBe("function (a, b) {\r\n      //An internal comment\r\n      return a * b;\r\n    }");
       
   });    
 });
